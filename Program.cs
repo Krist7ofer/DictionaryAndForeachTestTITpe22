9e9ddded48fec65260604352d145556062bee75b
opilane <opilane@HP-CZC0359NNZ>
2023-02-21 08:43:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DictionaryAndForeachTestTITpe22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var koolid = new Dictionary<int, string>()
+            {
+                {1,"Tallinna Reaalkool" },
+                {2,"Tallinna Tööstushariduskeskus" },
+                {3,"Tallinna Ühisgümnaasium" }
+            };
+            foreach (var kool in koolid)
+            {
+                Console.WriteLine($"{kool.Key} - {kool.Value}");
+            }
+        }
+    }
+}
